ace618dc23b4ea07592b710924a6196e77c7388d
bonepeople <bonepeople@sina.com>
2022-01-07 21:11:00 +0800

    为示例程序添加生成AppSecret的方法

--- simple/src/main/java/com/bonepeople/android/shade/simple/MainActivity.kt
+++ simple/src/main/java/com/bonepeople/android/shade/simple/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.bonepeople.android.shade.Lighting
 import com.bonepeople.android.shade.simple.databinding.ActivityMainBinding
+import com.bonepeople.android.widget.util.AppEncrypt
 import com.bonepeople.android.widget.util.AppLog
 
 class MainActivity : AppCompatActivity() {
@@ -13,6 +14,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(views.root)
         views.buttonConfig.setOnClickListener { config() }
         views.buttonSave.setOnClickListener { save() }
+        views.buttonGenerate.setOnClickListener { generate() }
     }
 
     private fun config() {
@@ -23,5 +25,20 @@ class MainActivity : AppCompatActivity() {
     private fun save() {
         AppLog.debug("save")
         Lighting.save(1, 1, "test", "123")
+    }
+
+    private fun generate() {
+        val packageName = "com.bonepeople.android.shade.simple"
+        val path = "http://192.168.1.200:8192/app/karo"
+        val secret = "vH6mR9rw2qXTlv2J"
+        val salt = "jUE5HeW12q33uMqx"
+
+        val content = """{"path":"$path","secret":"$secret","salt":"$salt"}"""
+        val md5 = AppEncrypt.encryptByMD5(packageName)
+        val encrypt = AppEncrypt.encryptByAES(content, md5.substring(0, 16), md5.substring(16, 32))
+        AppLog.debug("encrypt =\n$encrypt")
+
+        val decrypt = AppEncrypt.decryptByAES(encrypt, md5.substring(0, 16), md5.substring(16, 32))
+        AppLog.debug(decrypt)
     }
 }

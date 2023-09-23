9d9b26d6ec79512313abc35ef6b1d563ae86c1e0
bonepeople <bonepeople@sina.com>
2023-09-23 16:42:11 +0800

    调整示例APP，删除不再使用的功能

--- simple/src/main/java/com/bonepeople/android/shade/simple/MainActivity.kt
+++ simple/src/main/java/com/bonepeople/android/shade/simple/MainActivity.kt
@@ -3,10 +3,8 @@ package com.bonepeople.android.shade.simple
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.bonepeople.android.shade.Lighting
-import com.bonepeople.android.shade.Protector
 import com.bonepeople.android.shade.simple.databinding.ActivityMainBinding
 import com.bonepeople.android.widget.CoroutinesHolder
-import com.bonepeople.android.widget.util.AppToast
 import com.bonepeople.android.widget.util.AppView.singleClick
 import kotlinx.coroutines.launch
 
@@ -15,24 +13,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         val views = ActivityMainBinding.inflate(layoutInflater)
         setContentView(views.root)
-        views.buttonConfig.singleClick { config() }
         views.buttonSave.singleClick { save() }
-        views.buttonGenerate.singleClick { generate() }
-    }
-
-    private fun config() {
-        AppToast.show("App启动后会自动发送注册请求")
     }
 
     private fun save() {
         CoroutinesHolder.io.launch {
             Lighting.c5("shade.simple.test", 1, "test", "测试save函数")
-        }
-    }
-
-    private fun generate() {
-        Protector.protect {
-            AppToast.show("当前版本不需要生成配置信息")
         }
     }
 }

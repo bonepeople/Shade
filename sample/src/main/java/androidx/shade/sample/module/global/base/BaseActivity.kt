ad1d0c2ad080b8a1847e6223b3fd2d6f3aa017f4
bonepeople <bonepeople@qq.com>
2025-10-04 09:17:58 +0800

    Make system bar icon appearance configurable in BaseActivity

--- sample/src/main/java/androidx/shade/sample/module/global/base/BaseActivity.kt
+++ sample/src/main/java/androidx/shade/sample/module/global/base/BaseActivity.kt
@@ -7,11 +7,17 @@ import androidx.core.view.WindowCompat
 import androidx.fragment.app.FragmentActivity
 
 open class BaseActivity : FragmentActivity() {
+    protected open val isAppearanceLightSystemBars: Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         WindowCompat.setDecorFitsSystemWindows(window, false)
         window.statusBarColor = Color.TRANSPARENT
         window.navigationBarColor = Color.TRANSPARENT
+        WindowCompat.getInsetsController(window, window.decorView)?.apply {
+            isAppearanceLightStatusBars = isAppearanceLightSystemBars
+            isAppearanceLightNavigationBars = isAppearanceLightSystemBars
+        }
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
             window.isNavigationBarContrastEnforced = false
         }

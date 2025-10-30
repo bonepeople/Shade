0d4eae61d483661bb45bdd12dd4305f84c06e433
bonepeople <bonepeople@qq.com>
2025-10-30 19:33:38 +0800

    Migrate start screen text to StringTemplate
    - Add English and Simplified Chinese start screen templates.

---
+++ sample/src/main/java/androidx/shade/sample/module/start/text/StartTextEnUS.kt
@@ -0,0 +1,8 @@
+package androidx.shade.sample.module.start.text
+
+class StartTextEnUS : StartText() {
+    override val appName: String = "ShadeSample"
+    override val tagline: String = "A lightweight Android application protection library."
+    override val versionFormat: String = "Version %s"
+    override val initError: String = "Initialization failed"
+}
---
+++ sample/src/main/java/androidx/shade/sample/module/start/text/StartTextZhCN.kt
@@ -0,0 +1,8 @@
+package androidx.shade.sample.module.start.text
+
+class StartTextZhCN : StartText() {
+    override val appName: String = "ShadeSample"
+    override val tagline: String = "一个轻量级的 Android 应用保护库。"
+    override val versionFormat: String = "版本 %s"
+    override val initError: String = "初始化失败"
+}

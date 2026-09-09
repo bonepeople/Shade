5e73ae0fe5d6512e0d6fd47c127434e1fe516702
bonepeople <bonepeople@qq.com>
2026-09-09 16:57:17 +0800

    Add BaseActivity with edge-to-edge support

--- sample/src/main/java/androidx/shade/sample/module/home/HomeActivity.kt
+++ sample/src/main/java/androidx/shade/sample/module/home/HomeActivity.kt
@@ -1,12 +1,14 @@
 package androidx.shade.sample.module.home
 
 import android.os.Bundle
-import androidx.fragment.app.FragmentActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.lifecycleScope
 import androidx.shade.EarthTime
 import androidx.shade.Lighting
 import androidx.shade.Protector
 import androidx.shade.sample.databinding.ActivityMainBinding
+import androidx.shade.sample.module.global.base.BaseActivity
 import androidx.shade.sample.module.home.text.HomeText
 import com.bonepeople.android.widget.CoroutinesHolder
 import com.bonepeople.android.widget.resource.StringResourceManager
@@ -15,13 +17,18 @@ import com.bonepeople.android.widget.util.AppView.singleClick
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
-class HomeActivity : FragmentActivity() {
+class HomeActivity : BaseActivity() {
     private val views: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Protector.protect {
             setContentView(views.root)
+            ViewCompat.setOnApplyWindowInsetsListener(views.root) { view, insets ->
+                val systemBarInsets = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+                view.setPadding(systemBarInsets.left, systemBarInsets.top, systemBarInsets.right, systemBarInsets.bottom)
+                insets
+            }
             views.buttonSave.singleClick { save() }
         }
         updatePageText()
--- sample/src/main/java/androidx/shade/sample/module/start/StartActivity.kt
+++ sample/src/main/java/androidx/shade/sample/module/start/StartActivity.kt
@@ -1,36 +1,27 @@
 package androidx.shade.sample.module.start
 
 import android.content.Intent
-import android.graphics.Color
-import android.os.Build
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.core.view.ViewCompat
-import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.flowWithLifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.shade.sample.R
 import androidx.shade.sample.databinding.ActivityStartBinding
+import androidx.shade.sample.module.global.base.BaseActivity
 import androidx.shade.sample.module.home.HomeActivity
 import com.bonepeople.android.widget.ApplicationHolder
 import com.bonepeople.android.widget.util.AppToast
 import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.launch
 
-class StartActivity : FragmentActivity() {
+class StartActivity : BaseActivity() {
     private val views: ActivityStartBinding by lazy { ActivityStartBinding.inflate(layoutInflater) }
     private val viewModel: StartViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        WindowCompat.setDecorFitsSystemWindows(window, false)
-        window.statusBarColor = Color.TRANSPARENT
-        window.navigationBarColor = Color.TRANSPARENT
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            window.isNavigationBarContrastEnforced = false
-        }
         setContentView(views.root)
         ViewCompat.setOnApplyWindowInsetsListener(views.root) { view, insets ->
             val systemBarInsets = insets.getInsets(WindowInsetsCompat.Type.systemBars())
